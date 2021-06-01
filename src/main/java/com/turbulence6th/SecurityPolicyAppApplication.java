5c2d2906597f35e3d965a9e2de7055a600e8c355
Oğuz Tanrıkulu <turbulence6th@gmail.com>
2021-06-01 20:27:18 +0300

    Initial commit

---
+++ src/main/java/com/turbulence6th/SecurityPolicyAppApplication.java
@@ -0,0 +1,12 @@
+package com.turbulence6th;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityPolicyAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityPolicyAppApplication.class, args);
+	}
+}
---
+++ src/test/java/com/turbulence6th/SecurityPolicyAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.turbulence6th;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityPolicyAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
